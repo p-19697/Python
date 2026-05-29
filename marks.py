cad47d2b7f4b7d0e7100c4627b0c7c257a1dc327
pranjali Gursude <pranjaligursude@gmail.com>
2026-05-29 22:29:57 +0530

    messege

---
+++ marks.py
@@ -0,0 +1,23 @@
+sub1 = int(input("Enter sub 1 maarks:"))
+sub2 = int(input("Enter sub 2 maarks:"))
+sub3 = int(input("Enter sub 3 maarks:"))
+sub4 = int(input("Enter sub 4 maarks:"))
+sub5 = int(input("Enter sub 5 maarks:"))
+
+total= sub1 + sub2 + sub3 + sub4 + sub5 
+percentage = total/5
+
+print("Total marks :",total)
+print("Percenntage :",percentage)
+
+
+if percentage >= 70:
+            print("Result = Distinction")
+
+elif percentage >= 60:
+            print("Result = first class")
+elif percentage >= 45:
+            print("Result = pass")
+else:
+            print("Result = Fail")
+
