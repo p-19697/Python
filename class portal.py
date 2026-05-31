3f0b68b51014b231891657bf16c8fce3f5fe8f1e
pranjali Gursude <pranjaligursude@gmail.com>
2026-05-31 23:51:22 +0530

    Day 3 : project

---
+++ class portal.py
@@ -0,0 +1,32 @@
+#collage smart portal project
+#Student details
+student=["Pranjali","Aarti","Dhanashree","Pavan"]
+roll_no=[101,102,103,104]
+marks=[98,99,95,90]
+attendence=[68,70,75,80]
+
+notice=[ "Semester Exam starts from 15 June",
+    "Submit assignment before 10 June",
+    "College fest on 20 June"
+    ]
+
+def student_login():
+    name=input("Enter student name :")
+    rollno=int(input("Enter student roll number :"))
+    for i in range(len(student)):
+
+        if student[i]==name and roll_no[i]==rollno:
+            print("Login successfully")
+            print ("Welcome to the student portal")
+            print("***Student Details***")
+            print("Name:",name)
+            print("Roll No:",roll_no[i])
+            print("Marks:",marks[i])
+            print("Attendence:",attendence[i])
+            print("Notice Board")
+            print(notice)
+            return 
+        else :
+             print("login unsuccesssfully")
+             
+student_login()    
---
+++ list.py
@@ -0,0 +1,10 @@
+students =["dipak","kartik","satyarth","satyarth","satyarth"]
+print(students)
+print (students[0])
+print (students[1])
+print (students[2])
+print (students[3])
+print (students[4])
+
+for student in students:
+    print(f"hello{student}! Welcome to the class")
