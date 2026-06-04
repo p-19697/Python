4060963ed22a4347707889258b4c38b4d704df44
pranjali Gursude <pranjaligursude@gmail.com>
2026-06-05 00:01:07 +0530

    Day 5 :first flask app with routes

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
-from flask import Flask, render_template
-import flask
+from flask import Flask
 
-app= Flask(__name__)
+app= Flask (__name__)
 
-#project data - dictionary dynamic
-
-studentss=[
-    {"name":"Rahul","marks":95,"Roll no":1},
-    {"name":"Satyarth","marks":85,"Roll no":2},
-    {"name":"Pratik","marks":75,"Roll no":3},
-    {"name":"Kartik","marks":65,"Roll no":4},
-
+#project data - dynamic data insert 
+stud=[
+    {"name":"Dipak","roll no":101,"marks":98},
+    {"name":"Aarti","roll no":102,"marks":99},
+    {"name":"Pranjali","roll no":103,"marks":97},
+    {"name":"Rohit","roll no":104,"marks":93},
 ]
 
 @app.route('/')
 def home():
-    #Creat using HTml
-    return  render_template ("home.html")
+    html = '<h1>College Smart Portal - Students</h1>'
+    html += '<ul>'
 
+    for student in stud:
+        html += f"<li>{student['name']} - Roll No: {student['roll no']} - Marks: {student['marks']}</li>"
 
-    return '<h1>Colage Portal</h1>'
+    html += '</ul>'
+    return html 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return '<h1>About college smart portal<h1><p>This is college management system</p>'
 
-@app.route('/Students')
-def Students():
-    return render_template('Students.html', students=studentss)
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route('/student')
+def student():
+    return '<h1> student list<h1> <p> All students will be shown here</p>'
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
