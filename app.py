79df8cb6d9ca7f67f2c301d9e0eb93ae5a541d5a
pranjali Gursude <pranjaligursude@gmail.com>
2026-06-07 23:27:06 +0530

    Day 6 :home, records and notice

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Student Records
-stud = [
-    {"name": "Pranjali", "roll": 1, "marks": 96},
-   {"name": "Aarti", "roll": 2, "marks": 90},
-   {"name": "Dhanasree", "roll": 3, "marks": 89},
-   {"name": "Poonam1", "roll": 4, "marks": 88},
+students = [
+    {"department": "Computer", "enrollment": 24510290065, "name": "Pranjali", "roll": 101},
+    {"department": "Electronics", "enrollment": 24510290066, "name": "Aarti", "roll": 102},
+    {"department": "Mechanical", "enrollment": 24510290067, "name": "Dhanashree", "roll": 103},
+    {"department": "Electronics", "enrollment": 24510290068, "name": "Poonam", "roll": 104},
+    {"department": "Computer", "enrollment": 24510290069, "name": "Kaveri", "roll": 105},
+
 ]
 
-# Route 1 - Homepage
+notices = [
+    "1st Unit Test starts from 15 June 2026",
+    "2nd Unit Test starts from 6 Augst 2026",
+    "Project Submission Date: 20 June 2026",
+    "College Annual Function on 25 June 2026",
+]
+
 @app.route('/')
 def home():
     return render_template('home.html')
-    
-# Route 2 - Student Records
-@app.route('/about')
-def about():
-      return render_template('about.html')
 
-# Route 3 - Notice Board
-@app.route('/student')
-def student():
-    return render_template('student.html', students=stud)
+@app.route('/records')
+def records():
+    return render_template('records.html', students=students)
 
-if __name__ == '__main__':
-     print("INSIDE MAIN")
-     app.run(debug=True)
+@app.route('/notice')
+def notice():
+    return render_template('notice.html', notices=notices)
 
+if __name__ == "__main__":
+    app.run(debug=True)
