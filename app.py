fe93c0459888927bd5848fd2dc61db43620bb603
pranjali Gursude <pranjaligursude@gmail.com>
2026-06-08 19:14:29 +0530

    Day 3 : using css

--- app.py
+++ app.py
@@ -3,32 +3,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 students = [
-    {"department": "Computer", "enrollment": 24510290065, "name": "Pranjali", "roll": 101},
-    {"department": "Electronics", "enrollment": 24510290066, "name": "Aarti", "roll": 102},
-    {"department": "Mechanical", "enrollment": 24510290067, "name": "Dhanashree", "roll": 103},
-    {"department": "Electronics", "enrollment": 24510290068, "name": "Poonam", "roll": 104},
-    {"department": "Computer", "enrollment": 24510290069, "name": "Kaveri", "roll": 105},
-
+    {   "id": 1, "name": "Pranjali Gursude", "department": "Computer Engineering","attendance": "92%", "marks": 88 },
+    {  "id": 2, "name": "Aarti More", "department": "Mechanical Engineering", "attendance": "85%", "marks": 80 },
+    {  "id": 3,  "name": "Dhanashree Pole", "department": "Electronics Engineering","attendance": "90%", "marks": 91},
+    {"id": 4,"name": "Poonam Kurhe","department": "Computer Engineering","attendance": "98%","marks": 99 },
+    {"id": 5,"name": "Gauri Deshmukh","department": "Electronics Engineering","attendance": "90%","marks": 95},
+    {"id": 6,"name": "kaveri Wankhede","department": "Mechanical Engineering","attendance": "99%","marks": 99},
+    {"id": 7,"name": "Vaibhavi patil","department": "Computer Engineering","attendance": "90%","marks": 91}
 ]
 
-notices = [
-    "1st Unit Test starts from 15 June 2026",
-    "2nd Unit Test starts from 6 Augst 2026",
-    "Project Submission Date: 20 June 2026",
-    "College Annual Function on 25 June 2026",
-]
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/records')
+@app.route("/records")
 def records():
-    return render_template('records.html', students=students)
+    return render_template("records.html", students=students)
 
-@app.route('/notice')
-def notice():
-    return render_template('notice.html', notices=notices)
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/record/<int:id>")
+def record(id):
+    student = None
+
+    for s in students:
+        if s["id"] == id:
+            student = s
+            break
+
+    return render_template("record_detail.html", student=student)
 
 if __name__ == "__main__":
     app.run(debug=True)
